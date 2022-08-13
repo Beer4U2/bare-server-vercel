03abb0b6abf43f4682eb79c818cb156bbb1a02a6
illusions <97645434+illusionTBA@users.noreply.github.com>
2022-08-13 19:59:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 import createBareServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import express from 'express';
-import cors from 'cors';
-const app = express();
-app.use(cors())
-const httpServer = http.createServer(app);
 
+const httpServer = http.createServer();
 
-const bareServer = createBareServer('/', {
-    logErrors: false,
-    localAddress: undefined,
-    maintainer: {
-        email: 'illusionztba@gmail.com',
-        website: 'https://github.com/illusionTBA/bare-server-vercel',
-    },
+const expressServer = express();
+
+expressServer.get('/', (req, res) => {
+	res.send('your looking for the <a href="/bare/">bare</a> route');
+});
+
+const bareServer = createBareServer('/bare/', {
+	logErrors: false,
+	localAddress: undefined,
+	maintainer: {
+		email: 'tomphttp@sys32.dev',
+		website: 'https://github.com/tomphttp/',
+	},
 });
 
 httpServer.on('request', (req, res) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeRequest(req, res);
-    } else {
-        expressServer(req, res);
-    }
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		expressServer(req, res);
+	}
 });
 
 httpServer.on('upgrade', (req, socket, head) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
-    }
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 });
 
 httpServer.on('listening', () => {
-    console.log('HTTP server listening');
+	console.log('HTTP server listening');
 });
 
 httpServer.listen({
-    port: 3000,
+	port: 8080,
 });
